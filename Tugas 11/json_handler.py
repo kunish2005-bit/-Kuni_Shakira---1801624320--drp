9f1521a87edc8a42937c660b1e2228868daec690
osarara <osaosarara@gmail.com>
2026-07-03 17:29:31 +0700

    Menambahkan fitur export JSON

--- Tugas 11/json_handler.py
+++ Tugas 11/json_handler.py
@@ -1,8 +1,18 @@
-import json
 import sqlite3
+import json
 
 def export_json():
-    print("Fitur Export JSON")
+    
+    conn = sqlite3.connect("StudiO.db")
+    cursor = conn.cursor() 
 
-def import_json():
-    print("Fitur Import JSON")
+    cursor.execute("SELECT * FROM studiO")
+    data = cursor.fetchall()
+
+    with open("study.json","w") as file:
+        json.dump(data, file)
+
+    conn.close()
+
+    print("Data berhasil di export")
+
